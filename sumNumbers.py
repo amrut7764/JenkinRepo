b9a0ddf55f1b2b0518b7e6cbd55d132b55afa917
Amrut Chougale <chougale.amrut@gmail.com>
2019-01-10 19:46:54 -0500

    Add Maths class

--- sumNumbers.py
+++ sumNumbers.py
@@ -1,8 +1,14 @@
 #!/usr/local/bin/python
 
-def sum(number1, number2):
-    return number1 + number2
+class Maths(object):
+    def __init__(self):
+        pass
+
+    def sum(self, number1, number2):
+        return number1 + number2
+
 
 if __name__ == "__main__":
-    ret = sum(10 , 20)
+    py = Maths()
+    ret = py.sum(10 , 20)
     print ret
